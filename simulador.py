71ca94e773a6490b4ab76182480b4a0e2a3948ef
mathiasbelle <mathiasbelle@hotmail.com>
2018-05-11 17:25:05 -0300

    Logica principal pronta

--- simulador.py
+++ simulador.py
@@ -9,32 +9,7 @@ class Simulacao:
     T_MORTO = 0
     TAMANHO = 0
     qtd_paginas = 0
-'''
-def lerArquivo(caminho):
-    try:
-        arquivo = open(caminho, "r")
-    except Exception as ex:
-        janela = Tk()
-        container = Frame(janela, height = 500)
-        container.pack()
-        janelaErro = Label(container, text = type(ex), font = 100)
-        janelaErro.pack()
-        return False
-    listaProcessos = []
-    
-    for lena in arquivo:
-        processoAtual = Simulacao()
-        linha = lena.split(",")
-        processoAtual.ID = int(linha[0])
-        processoAtual.T_CRIADO = int(linha[1])
-        processoAtual.T_MORTO = int(linha[2])
-        processoAtual.TAMANHO = int(linha[3])
-        listaProcessos.append(processoAtual)
-        
-    for x in range(0,len(listaProcessos)):
-        print("\nID: ",listaProcessos[x].ID,"\nT_CRIADO: ",listaProcessos[x].T_CRIADO,"\nT_MORTO: ",listaProcessos[x].T_MORTO,"\nTAMANHO: ",listaProcessos[x].TAMANHO)
-'''
-
+    tempoMorrer = 0
 
 class Aplication:
 
@@ -114,7 +89,7 @@ class Aplication:
             processoAtual.qtd_paginas = math.ceil(processoAtual.TAMANHO/int(self.tamanhosPaginaStrings.get()))
 
             self.listaProcessos.append(processoAtual)
-        
+        arquivo.close()
         #for x in range(0,len(self.listaProcessos)):
         #    print("\nID: ",self.listaProcessos[x].ID,"\nT_CRIADO: ",self.listaProcessos[x].T_CRIADO,"\nT_MORTO: ",self.listaProcessos[x].T_MORTO,"\nTAMANHO: ",self.listaProcessos[x].TAMANHO, "\nQtd páginas: ", processoAtual.qtd_paginas)
 
@@ -161,93 +136,55 @@ class Aplication:
         #filaEspera = []
         tempoTotal = 1
         tempoEspera = 0
-        tempoProcessos = 1
-        cond = True
-        lol = 0
+        #tempoProcessos = 1
+        #cond = True
         print("tempo | processo | posicao_inicial_na_memoria | ação")
         input()
-        while(cond):
-            #print("Quantidade de processos dentro: ",len(processosDentro))
+        while(True):
             print("Quantidade de páginas disponíveis: ",qtdPaginas)
-            #print("TEMPO TOTAL: ", contadorTempo, "\nQuantidade de páginas disponíveis: ",qtdPaginas)
-            #input()
-            #print("\n TEMPO: ", contadorTempo)
-            cond2 = True
+            #cond2 = True
             #x = 0
-            while(cond2):
-                #print("Contador tempo: ", contadorTempo)
-                #print("x = ",x,"\nContador tempo: ", contadorTempo, "\nQuantidade de páginas: ",qtdPaginas)
-                #print("TEMPO: ", contadorTempo, "\nQuantidade de páginas: ",qtdPaginas)
-                #print("Processo atual = ", self.listaProcessos[0].ID)
-                #print("\nID: ",self.listaProcessos[0].ID,"\nT_CRIADO: ",self.listaProcessos[0].T_CRIADO,"\nT_MORTO: ",self.listaProcessos[0].T_MORTO,"\nTAMANHO: ",self.listaProcessos[0].TAMANHO)
-                #if(self.listaProcessos[0].T_CRIADO <= tempoTotal): #and self.listaProcessos[0].qtd_paginas <= qtdPaginas
-                    #print("ID: ",self.listaProcessos[0].ID,"\nT_CRIADO: ",self.listaProcessos[0].T_CRIADO,"\nT_MORTO: ",self.listaProcessos[0].T_MORTO,"\nTAMANHO: ",self.listaProcessos[0].TAMANHO)
-                if(self.listaProcessos[0].qtd_paginas <= qtdPaginas and self.listaProcessos[0].T_CRIADO <= tempoProcessos):
-                    #print("TEMPO TOTAL: ", contadorTempo, "\nQuantidade de páginas disponíveis: ",qtdPaginas)
-                    #print("Tempo processos: ", tempoProcessos)
-                    #print("ID: ",self.listaProcessos[0].ID,"\nT_CRIADO: ",self.listaProcessos[0].T_CRIADO)#"\nT_MORTO: ",self.listaProcessos[0].T_MORTO,"\nTAMANHO: ",self.listaProcessos[0].TAMANHO)
-                    #print("Processo ID ",self.listaProcessos[0].ID," entrou.")
-                    if(lol == 1):
-                        tempoProcessos+=1
-                    print("Tempo quando entrou: ",tempoTotal," Processo: ",self.listaProcessos[0].ID," NULL"," Entrou")
-                    if(self.listaProcessos[0].T_CRIADO == self.listaProcessos[0].T_MORTO):
-                        print("Tempo quando saiu: ",tempoTotal," Processo: ",self.listaProcessos[0].ID," NULL","|| Saiu")
-                        self.listaProcessos.pop(0)
+            while(True):
+                if self.listaProcessos: # Enquanto processos estiverem esperando
+                    if(self.listaProcessos[0].qtd_paginas <= qtdPaginas):
+                        if(self.listaProcessos[0].T_CRIADO <= tempoTotal):
+
+                            print("Tempo quando entrou: ",tempoTotal," Processo: ",self.listaProcessos[0].ID," NULL"," Entrou")
+                            if(self.listaProcessos[0].T_CRIADO == self.listaProcessos[0].T_MORTO):
+                                print("Tempo quando saiu: ",tempoTotal," Processo: ",self.listaProcessos[0].ID," NULL","|| Saiu")
+                                self.listaProcessos.pop(0)
+                            else:
+                                qtdPaginas -= self.listaProcessos[0].qtd_paginas
+                                self.listaProcessos[0].tempoMorrer = tempoTotal + (self.listaProcessos[0].T_MORTO - self.listaProcessos[0].T_CRIADO)
+                                processosDentro.append(self.listaProcessos.pop(0))
+                                #tempoTotal += 1
                     else:
-                        qtdPaginas -= self.listaProcessos[0].qtd_paginas
-                        processosDentro.append(self.listaProcessos.pop(0))
-                    #contadorTempo+=1
-                    lol = 1
-                    if(self.listaProcessos[0].T_CRIADO > tempoProcessos):
-                        #print("Processo pular:",self.listaProcessos[0].ID)
-                        while(self.listaProcessos[0].T_CRIADO != tempoProcessos):
-                            tempoProcessos+=1
-                            #print("Tempo = ",tempoProcessos)
-                        lol = 0
+                        print("Tempo: ",tempoTotal," Processo: ",self.listaProcessos[0].ID," NULL"," Esperando", "Tempo de espera total: ", tempoEspera)
+                        tempoEspera += 1
+                        #tempoTotal += 1
                         break
-                    #tempoProcessos+=1
-                    #tempoEspera = 0
-                    #print("Quantidade de processos dentro: ",len(processosDentro))
+                    if self.listaProcessos: # Caso seja o último processo, vai tentar acessar a primeira posição da lista, que já está vazia, logo o if para verificar
+                        if(self.listaProcessos[0].T_CRIADO != tempoTotal): # Caso não tenha mais nenhum processo a ser criado no mesmo tempo sai do loop
+                            break
                 else:
-                    #print("Tempo processos: ", tempoProcessos)
-                    #print("Sem espaço para o processo ",self.listaProcessos[0].ID,", processo esperando.")
-                    #print("Tempo de espera: ",tempoEspera)
-                    print("Tempo: ",tempoTotal," Processo: ",self.listaProcessos[0].ID," NULL"," Esperando", "||Tempo processos: ",tempoProcessos)
-                    tempoEspera+=1
                     break
-                    #filaEspera.append(self.listaProcessos[0])
-                    #contadorTempo+=1
-                    #x+=1
-                    #print("XX = ",x)
-                #else:
-                #    cond2 = False
-                #x+=1
-            tempoTotal = tempoEspera + tempoProcessos
-            #print("Tempo total = ", tempoTotal)
             if processosDentro:
-                #print("Tempo total = ", tempoTotal)
-                #print("Quantidade de processos dentro: ",len(processosDentro))
-                #for a in range(len(processosDentro)):
-                cond3 = True
                 a = 0
-                while(cond3):
+                while(True):
                     if(a >= len(processosDentro)):
                         break
-                    if(processosDentro[a].T_MORTO <= tempoProcessos):
+                    if(processosDentro[a].tempoMorrer == tempoTotal):
                         qtdPaginas += processosDentro[a].qtd_paginas
-                        #print("Tempo de morrer: ", processosDentro[a].T_MORTO,"\ntempoProcessos: ",tempoProcessos)
-                        #print("Processo ID = ",processosDentro[a].ID," saiu.")
                         print("Tempo quando saiu: ",tempoTotal," Processo: ",processosDentro[a].ID," NULL","|| Saiu")
                         processosDentro.pop(a)
                         a-=1
                     a+=1 
-            #tempoTotal = contadorTempo+tempoEspera
-            #contadorTempo+=1
 
-            if not self.listaProcessos and processosDentro:
-                cond = False
-            
+            if not self.listaProcessos and not processosDentro: # Quando todos os processos já tiverem entrado e saído
+                break
+            tempoTotal += 1
             input()
+        print("Fim da Simulação")
     
 root = Tk()
 Aplication(root)
